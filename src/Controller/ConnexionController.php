34cade86700236111059fa759b02bf2a404b2c1d
Lucas <lucas.chancel@laposte.net>
2024-11-26 10:38:27 +0100

    add vue  page connexion

---
+++ src/Controller/ConnexionController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ConnexionController extends AbstractController
+{
+
+  #[Route('/', name: 'home')]
+  public function index(Request $request): Response
+  {
+    return $this->render('connexion/connexion.html.twig');
+  }
+
+}
